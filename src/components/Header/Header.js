ecaa8ab7f391d11136d8961157ab3e15479ed5c4
mihaibuga <77620547+mihaibuga@users.noreply.github.com>
2023-02-27 00:07:53 +0200

    Use basic routing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <header className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <a className="navbar-brand" href="#">
+                <Link className="navbar-brand" to="/">
                     Portfolio Website
-                </a>
+                </Link>
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -19,26 +21,14 @@ const Header = () => {
                 <nav className="collapse navbar-collapse" id="navbarContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a
-                                className="nav-link"
-                                aria-current="page"
-                                href="#"
-                            >
+                            <Link className="nav-link" aria-current="page" to="/">
                                 Home
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                     <form className="d-flex">
-                        <input
-                            className="form-control me-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                        />
-                        <button
-                            className="btn btn-outline-success"
-                            type="submit"
-                        >
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">
                             Search
                         </button>
                     </form>
