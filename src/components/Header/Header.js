756a60a9032baa204d341890f95e8012b7f5e2b4
mihaibuga <77620547+mihaibuga@users.noreply.github.com>
2023-03-05 15:27:49 +0200

    Style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,36 +4,18 @@ import "./header.scss";
 
 const Header = () => {
     return (
-        <header className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <header>
             <div className="container">
                 <Link className="navbar-brand" to="/">
                     Portfolio Website
                 </Link>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarContent"
-                    aria-controls="navbarContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <nav className="collapse navbar-collapse" id="navbarContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link" aria-current="page" to="/">
-                                Home
-                            </Link>
-                        </li>
-                    </ul>
-                    <form className="d-flex">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">
-                            Search
-                        </button>
-                    </form>
+                <nav>
+                    <Link className="nav-link" aria-current="page" to="/">
+                        Home
+                    </Link>
+                    <Link className="nav-link" aria-current="page" to="/projects">
+                        Projects
+                    </Link>
                 </nav>
             </div>
         </header>
