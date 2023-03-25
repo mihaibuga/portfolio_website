7010f550e9e801985382314f2f55addf4861d3db
mihaibuga <77620547+mihaibuga@users.noreply.github.com>
2023-03-25 20:21:40 +0200

    Add social icons and style footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,27 @@
+import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
+
 import "./footer.scss";
 
 const Footer = () => {
-    const year = new Date().getFullYear();
+    const style = { height: 35, width: 35 };
+    const icon_bg_color = "#f9feff";
 
     return (
         <footer>
             <div className="container">
-                <span className="navbar-text">&copy; {year}</span>
+                <div class="social-icons">
+                    <SocialIcon url="mailto:johndoe@email.com" style={style} bgColor={icon_bg_color} />
+                    <SocialIcon url="https://www.linkedin.com/" style={style} bgColor={icon_bg_color} />
+                    <SocialIcon url="https://github.com/" style={style} bgColor={icon_bg_color} />
+                </div>
+
+                <div class="footer-brand-wrapper">
+                    <NavLink className="footer-brand" to="/">
+                        Portfolio Website
+                    </NavLink>
+                    <span className="copy">&copy; 2023</span>
+                </div>
             </div>
         </footer>
     );
