a08e857faaf4073c8ddb1b5737f82144a70a068f
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-02-02 23:40:56 +0100

    Add profile picture

--- src/data/details.js
+++ src/data/details.js
@@ -1,4 +1,4 @@
-import profileImage from "../assets/images/profile-image.png";
+import profileImage from "../assets/images/Profile/my-profile.jpeg";
 
 export const LNAME = process.env.REACT_APP_LNAME ? process.env.REACT_APP_LNAME : '';
 export const FNAME = process.env.REACT_APP_FNAME ? process.env.REACT_APP_FNAME : '';
@@ -7,4 +7,4 @@ export const YEAR = process.env.REACT_APP_YEAR ? process.env.REACT_APP_YEAR : ''
 export const LINKEDIN = process.env.REACT_APP_LINKEDIN ? process.env.REACT_APP_LINKEDIN : 'https://www.linkedin.com/';
 export const EMAIL = process.env.REACT_APP_EMAIL ? process.env.REACT_APP_EMAIL : '#';
 export const GITHUB = process.env.REACT_APP_GITHUB ? process.env.REACT_APP_GITHUB : 'https://github.com/';
-export const PROFILE_SRC = process.env.REACT_APP_PROFILE_SRC ? process.env.REACT_APP_PROFILE_SRC : profileImage;
+export const PROFILE_SRC = profileImage;
