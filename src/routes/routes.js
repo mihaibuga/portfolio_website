bcb952a73cd464d12dfd8ab1e25be08f0d3b1b05
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-03-17 15:50:38 +0100

    Created project details page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,29 +1,37 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Outlet, createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/HomePage/Home";
 import Projects from "../pages/ProjectsPage/Projects";
 import NotFound from "../pages/NotFoundPage/NotFound";
 import Contact from "../pages/ContactPage/Contact";
 import Resume from "../pages/ResumePage/Resume";
+import ProjectPage from "../pages/ProjectPage/Project";
 
 export const PATHS = {
-    landing: {path: "/", title: "Home"},
-    projects: {path: "/projects", title: "Projects"},
-    contact: {path: "/contact", title: "Contact"},
-    resume: {path: "/resume", title: "Resume"},
-    any: {path: "/*", title: "Any"},
+    landing: { path: "/", title: "Home" },
+    projects: { path: "/projects", title: "Projects" },
+    contact: { path: "/contact", title: "Contact" },
+    resume: { path: "/resume", title: "Resume" },
+    any: { path: "/*", title: "Any" },
 };
 
 export const router = createBrowserRouter([
     {
         path: PATHS.landing.path,
         element: <App />,
+        errorElement: <NotFound />,
         children: [
             { path: "", element: <Home /> },
-            { path: PATHS.projects.path, element: <Projects /> },
+            {
+                path: PATHS.projects.path,
+                element: <Outlet />,
+                children: [
+                    { index: true, element: <Projects /> },
+                    { path: ":id", element: <ProjectPage /> },
+                ],
+            },
             { path: PATHS.contact.path, element: <Contact /> },
             { path: PATHS.resume.path, element: <Resume /> },
-            { path: PATHS.any.path, element: <NotFound /> },
         ],
     },
 ]);
