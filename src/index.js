5506cd088534532668bc49c3e1dfa5ebc481769e
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2025-08-25 18:53:44 +0200

    Use Roboto font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from 'react-router-dom';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import {router} from "./routes/routes";
 import "./assets/styles/index.scss";
 
