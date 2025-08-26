54b44d8f778d7f2933123f376a770efedf12cf90
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2025-08-26 15:50:10 +0200

    Created and used data fetching hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import { Outlet } from "react-router";
-import useDataStore from "./store/dataStore";
-import { useEffect } from "react";
-import { getProfile, getSiteSettings } from "./utils/dataFetching";
+import { useFetchData } from "./utils/hooks";
 
 const App = () => {
-    const { storeProfileData, setStoreProfileData, storeSiteSettings, setStoreSiteSettings } = useDataStore();
-
-    useEffect(() => {
-        const fetchData = async () => {
-            if (storeProfileData === null) {
-                const fetchedProfile = await getProfile();
-                setStoreProfileData(fetchedProfile);
-            }
-
-            if (storeSiteSettings === null) {
-                const fetchedSiteSettings = await getSiteSettings();
-                setStoreSiteSettings(fetchedSiteSettings);
-            }
-        };
-
-        fetchData();
-    }, []);
+    useFetchData();
 
     return (
         <>
