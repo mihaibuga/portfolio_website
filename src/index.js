7a318800167fd433a6a5627bd5ab84f5e0e1f264
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2025-08-26 15:53:13 +0200

    Formatted index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider } from "react-router-dom";
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import {router} from "./routes/routes";
+import { router } from "./routes/routes";
 import "./assets/styles/index.scss";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
